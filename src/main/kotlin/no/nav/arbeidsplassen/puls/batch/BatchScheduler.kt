7f579395dca704fa38fb94d6c6ebab05ab857033
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2023-09-01 13:01:39 +0200

    Lager alert som fyrer dersom batchjobb ikke har kjørt på ett døgn

--- src/main/kotlin/no/nav/arbeidsplassen/puls/batch/BatchScheduler.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/puls/batch/BatchScheduler.kt
@@ -1,5 +1,6 @@
 package no.nav.arbeidsplassen.puls.batch
 
+import io.micrometer.core.instrument.MeterRegistry
 import io.micronaut.scheduling.annotation.Scheduled
 import jakarta.inject.Singleton
 import no.nav.arbeidsplassen.puls.LeaderElection
@@ -8,11 +9,14 @@ import org.slf4j.LoggerFactory
 @Singleton
 class BatchScheduler(
     private val batchFetchAmplitudeExport: BatchFetchAmplitudeExport,
-    private val leaderElection: LeaderElection
+    private val leaderElection: LeaderElection,
+    private val meterRegistry: MeterRegistry
 ) {
     companion object {
         private val LOG = LoggerFactory.getLogger(BatchScheduler::class.java)
     }
+
+    private val batchRunSuccess = meterRegistry.counter("pam.puls.batch.success")
 
     @Scheduled(fixedDelay = "16m")
     fun startFetchAmplitudeAndProcessEvents() {
@@ -21,6 +25,7 @@ class BatchScheduler(
             val time = System.currentTimeMillis()
             batchFetchAmplitudeExport.startBatchRunFetchExports()
             LOG.info("Batch was finished in ${(System.currentTimeMillis() - time) / 1000}s")
+            batchRunSuccess.increment()
         }
     }
 }
