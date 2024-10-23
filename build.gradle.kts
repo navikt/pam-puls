8e5780beb1a2448980eaede9b004bdcb23e4c9a5
haugse <sara.haugse@gmail.com>
2024-10-23 13:32:52 +0200

    Oppgraderer micronaut, java, gradle og andre dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "1.7.10"
-    kotlin("kapt") version "1.7.10"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.7.10"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.7.4"
+    kotlin("jvm") version "2.0.21"
+    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.0.21"
+    id("com.gradleup.shadow") version "8.3.2"
+    id("io.micronaut.application") version "4.4.2"
 }
 
 version = "0.1"
@@ -36,22 +38,24 @@ micronaut {
 }
 
 dependencies {
-    kapt("io.micronaut:micronaut-http-validation")
-    kapt("io.micronaut.data:micronaut-data-processor")
+    ksp("io.micronaut:micronaut-http-validation")
+    ksp("io.micronaut.data:micronaut-data-processor")
     implementation("io.micronaut:micronaut-http-client")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
-    implementation("javax.annotation:javax.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
     implementation ("jakarta.persistence:jakarta.persistence-api:${jakartaPersistenceVersion}")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     implementation("io.micronaut.kafka:micronaut-kafka:${micronautKafkaVersion}")
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("net.logstash.logback:logstash-logback-encoder:${logbackEncoderVersion}")
-    implementation("io.micronaut:micronaut-validation")
+    annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
+    implementation("io.micronaut.validation:micronaut-validation")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("io.micronaut:micronaut-jackson-databind")
     implementation("io.micronaut.micrometer:micronaut-micrometer-core")
     implementation("io.micronaut.micrometer:micronaut-micrometer-registry-prometheus")
     implementation("io.micronaut:micronaut-management")
@@ -59,6 +63,8 @@ dependencies {
     implementation("org.postgresql:postgresql:${postgresqlVersion}")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
     implementation("io.micronaut.flyway:micronaut-flyway")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.yaml:snakeyaml")
     testImplementation("io.micronaut.test:micronaut-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
     testImplementation("org.testcontainers:postgresql:${tcVersion}")
@@ -74,13 +80,13 @@ java {
 
 tasks {
     compileKotlin {
-        kotlinOptions {
-            jvmTarget = "$javaVersion"
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     compileTestKotlin {
-        kotlinOptions {
-            jvmTarget = "$javaVersion"
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     test {
--- src/main/kotlin/no/nav/arbeidsplassen/puls/outbox/OutboxRepository.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/puls/outbox/OutboxRepository.kt
@@ -6,11 +6,11 @@ import io.micronaut.data.model.Pageable
 import io.micronaut.data.model.query.builder.sql.Dialect
 import io.micronaut.data.repository.CrudRepository
 import io.micronaut.data.runtime.config.DataSettings
+import jakarta.transaction.Transactional
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.Statement
 import java.time.Instant
-import javax.transaction.Transactional
 
 @JdbcRepository(dialect = Dialect.POSTGRES)
 abstract class OutboxRepository(
